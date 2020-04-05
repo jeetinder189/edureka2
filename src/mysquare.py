495f2ea07625825f2ad0d4950298a8c7dec6e606
inder2 <jeetinder60@ymail.com>
2020-04-05 08:53:42 -0700

    chnges

--- src/mysquare.py
+++ src/mysquare.py
@@ -6,8 +6,6 @@ def my_square(x):
 
 print(my_square(5))
 print(my_square(50))
-<<<<<<< HEAD
 print(my_square(150))
-=======
 print(my_square(255))
->>>>>>> 0fca1b533a6ed6c1171a49cb1dc4d94d41643eb4
+
