f8f139d3b47cb1daa69b2c051a1c94156db56cb8
inder2 <jeetinder60@ymail.com>
2020-04-05 09:25:52 -0700

    (making commit on mybranch)

--- src/mysquare.py
+++ src/mysquare.py
@@ -8,4 +8,10 @@ print(my_square(5))
 print(my_square(50))
 print(my_square(150))
 print(my_square(255))
+print(my_square(5))
+print(my_square(4))
+print(my_square(3))
+print(my_square(2))
+print(my_square(1))
+
 
